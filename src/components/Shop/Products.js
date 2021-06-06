60da35f71820c0b0ca7d8d2dcdfb114b64ddfe21
ssonko94 <ssonkojimmy@gmail.com>
2021-06-06 12:10:25 +0300

    add some dummy product array to work with

--- src/components/Shop/Products.js
+++ src/components/Shop/Products.js
@@ -1,16 +1,34 @@
-import ProductItem from './ProductItem';
-import classes from './Products.module.css';
+import ProductItem from "./ProductItem";
+import classes from "./Products.module.css";
 
+const DUMMY_PRODUCTS = [
+  {
+    id: "p1",
+    title: "Kisses from Roman",
+    price: 10,
+    description: "My best  romance series ever!",
+  },
+  {
+    id: "p2",
+    title: "Star Wars Sword",
+    price: 25,
+    description: "The last gedi invisible sword",
+  },
+];
 const Products = (props) => {
   return (
     <section className={classes.products}>
       <h2>Buy your favorite products</h2>
       <ul>
-        <ProductItem
-          title='Test'
-          price={6}
-          description='This is a first product - amazing!'
-        />
+        {DUMMY_PRODUCTS.map((product) => (
+          <ProductItem
+            key={product.id}
+            id={product.id}
+            title={product.title}
+            price={product.price}
+            description={product.description}
+          />
+        ))}
       </ul>
     </section>
   );
